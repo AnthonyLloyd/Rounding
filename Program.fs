8c41ae65c86ed382cd93625a2190159aae4ac993
Anthony Lloyd <anthony@lloydhq.com>
2018-03-02 20:09:09 +0000

    fixed

--- Program.fs
+++ Program.fs
@@ -22,17 +22,33 @@ let ftestProp stdgen name = ftestPropertyWithConfig stdgen config name
 module Rounding =
     let inline private round (f:float) = int(if f<0.0 then f-0.5 else f+0.5)
     
-    let inline private absError (f:float) (wi:float) (ni:int) (d:int) =
-        let wc = f * float d
-        let wni = f * float ni
-        if (wc > 0.0 && wi <= wni) || (wc < 0.0 && wi >= wni) then abs(0.5 * f)
-        elif wi < wni then wi - f * (float ni + float d * 0.5) |> max 0.0
-        else f * (float ni + float d * 0.5) - wi |> max 0.0
+    let inline private absError (n:int) (wt:float) (wi:float) (ni:int) (d:int) =
+        let wc = wt * float d / float n
+        let wni = wt * float ni / float n
+        if (wc > 0.0 && wi <= wni) || (wc < 0.0 && wi >= wni) then abs(0.5 * wc)
+        elif wi < wni then wi - wni  - 0.5 * wc |> max 0.0
+        else wni + 0.5 * wc - wi |> max 0.0
 
-    let inline private relError f (wi:float) (ni:int) (d:int) =
-        absError f wi ni d / abs wi
+    let inline private relError n wt wi ni d =
+        absError n wt wi ni d / abs wi
 
     let distribute n weights =
+        let wt = Array.sum weights
+        if abs(Seq.maxBy abs weights * float(abs n+1)) >= abs wt * float Int32.MaxValue then None
+        else
+            let ns = Array.map ((*)(float n / wt) >> round) weights
+            let d = n - Array.sum ns
+            for __ = 1 to abs d do
+                let _,_,_,i = Seq.mapi2 (fun i wi ni ->
+                                            let e1 = absError n wt wi ni (sign d)
+                                            let e2 = relError n wt wi ni (sign d)
+                                            let e3 = if wt > 0.0 then -wi else wi
+                                            e1, e2, e3, i
+                                        ) weights ns |> Seq.min
+                ns.[i] <- ns.[i] + sign d
+            Some ns
+
+    let distributePrint n weights =
         let wt = Array.sum weights
         if abs(Seq.maxBy abs weights * float(abs n+1)) >= abs wt * float Int32.MaxValue then None
         else
@@ -40,14 +56,22 @@ module Rounding =
             let ns = Array.map ((*)f >> round) weights
             let d = n - Array.sum ns
             for __ = 1 to abs d do
+                Seq.mapi2 (fun i wi ni ->
+                                            let e1 = absError n wt wi ni (sign d)
+                                            let e2 = relError n wt wi ni (sign d)
+                                            let e3 = if wt > 0.0 then -wi else wi
+                                            e1, e2, e3, i
+                                        ) weights ns
+                |> Seq.sort |> Seq.toList |> printfn "%A"                            
+
                 let _,_,_,i = Seq.mapi2 (fun i wi ni ->
-                                            let e1 = absError f wi ni (sign d)
-                                            let e2 = relError f wi ni (sign d)
+                                            let e1 = absError n wt wi ni (sign d)
+                                            let e2 = relError n wt wi ni (sign d)
                                             let e3 = if wt > 0.0 then -wi else wi
                                             e1, e2, e3, i
                                         ) weights ns |> Seq.min
                 ns.[i] <- ns.[i] + sign d
-            Some ns
+            Some ns        
 let roundingTests =
     testList "rounding" [
         test "n zero" {
@@ -70,15 +94,15 @@ let roundingTests =
             let r = Rounding.distribute -100 [|-406.0;-348.0;-246.0;-0.0|]
             Expect.equal r (Some [|-40;-35;-25;0|]) "-40 etc"
         }
+        test "twitter tricky" {
+            let r = Rounding.distribute 100 [|404.0;397.0;47.0;47.0;47.0;58.0|]
+            Expect.equal r (Some [|40;39;5;5;5;6|]) "o no"
+        }
         test "negative example" {
             let r1 = Rounding.distribute 42 [|1.5;1.0;39.5;-1.0;1.0|]
             Expect.equal r1 (Some [|2;1;39;-1;1|]) "2 etc"
             let r2 = Rounding.distribute -42 [|1.5;1.0;39.5;-1.0;1.0|]
             Expect.equal r2 (Some [|-2;-1;-39;1;-1|]) "-2 etc"
-        }
-        test "problem" {
-            let r = Rounding.distribute 100 [|404.0;397.0;47.0;47.0;47.0;58.0|]
-            Expect.equal r (Some [|40;40;5;5;5;6|]) "o no"
         }
         testProp "n total correctly" (
             fun (n:int) (w:Gen.RationalFloat NonEmptyArray) ->
